fcf31126b31a6644528b8839fc5587b8702124b6
sidmadethis <sidmadethis@gmail.com>
2017-04-03 23:03:30 -0600

    worked with tuples

--- players.py
+++ players.py
@@ -1,2 +1,11 @@
 players = ['charles', 'martina', 'michael', 'florence','eli']
 print(players[0:3])
+print(players[:4])
+print(players[-3:])
+
+
+# omitting both first and second index numbers can be used to make a copy of a list
+
+my_foods=['pizza', 'falalfelfel', 'carrot cake']
+friends_food=my_foods[:]
+print(friends_food)
---
+++ tuples.py
@@ -0,0 +1,17 @@
+# tuple is an immutable list
+dimensions = (200,50)
+print(dimensions[0])
+print(dimensions[1])
+
+
+for dimension in dimensions:
+    print(dimension)
+
+
+buffet = ('pizza', 'french fries', 'breadsticks', 'ice cream' , 'soda')
+for item in buffet:
+    print(item)
+
+buffet = ('pizza', 'fish', 'chicken', 'ice cream', 'soda')
+for item in buffet:
+    print(item)
