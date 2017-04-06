ffec81de68d89e78d9f14ee6c8e489ff84a1d7a4
sidmadethis <sidmadethis@gmail.com>
2017-04-05 23:40:19 -0600

    if statements

--- if.py
+++ if.py
@@ -4,3 +4,9 @@ for car in cars:
         print(car.upper())
     else:
         print(car.title())
+
+
+banned_users = ['andrew', 'carolina', 'david']
+user = 'marie'
+if user not in banned_users:
+    print(user.title()+", you can post a response")
