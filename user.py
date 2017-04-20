a0ad93052d0815e3dfcb5d435bd86d9c8f0b98b1
sidmadethis <sidmadethis@gmail.com>
2017-04-20 09:23:35 -0600

    looping through keys and values

--- user.py
+++ user.py
@@ -16,6 +16,30 @@ favorite_languages = {
     'edward':'ruby',
     'phil':'python',
 }
-
+print("\n")
 for name,language in favorite_languages.items():
     print(name.title()+ "'s favorite language is " + language.title() + ".")
+
+
+print("\n")
+for name in favorite_languages.keys():
+    print(name.title())
+
+# this is the same as for name in favorite_languages:
+
+# use sort to get dictionary in order
+# note this does alphabetical sorting
+print("\n")
+for name in sorted(favorite_languages.keys()):
+    print(name.title() + ", thank you for taking our poll!")
+
+
+print("\n")
+print("the following langauges have been mentioned")
+for language in favorite_languages.values():
+    print(language.title())
+
+# use set to not have duplicates
+print("\nNo duplicates")
+for language in set(favorite_languages.values()):
+    print(language)
