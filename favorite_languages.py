adce29d2e2b4dbb38ebad5f42a349ff769eed27d
sidmadethis <sidmadethis@gmail.com>
2017-04-23 00:11:14 -0600

    dictionary in a dictionary

--- favorite_languages.py
+++ favorite_languages.py
@@ -9,3 +9,26 @@ for name, languages in favorite_languages.items():
     print("\n" + name.title()+ "'s favorite languages are:")
     for language in languages:
         print("\t" + language.title())
+
+# you can get too many layers and get complicated
+
+users = {
+    'aeinstein': {
+        'first': 'Albert',
+        'last': 'Einstein',
+        'location': 'princeton',
+    },
+    'mcurie': {
+        'first': 'marie',
+        'last': 'Curie',
+        'location': 'paris',
+    },
+}
+
+for username, user_info in users.items():
+    print("\nUsername: " + username)
+    full_name = user_info['first'] + " " + user_info['last']
+    location = user_info['location']
+
+    print("\tFull name: "+full_name.title())
+    print("\tLocation: "+location.title())
