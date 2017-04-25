0327302e6fce39a7e0cb4513ff75b9d6e4d6e4e0
sidmadethis <sidmadethis@gmail.com>
2017-04-24 23:43:27 -0600

    raw_input for python2.7  just input for python3

--- parrot.py
+++ parrot.py
@@ -1,2 +1,17 @@
+# message = raw_input("tell me something and I will repeat it back to you: ")
+# print(message)
+
 message = input("Tell me something and I will repeat it back to you: ")
 print(message)
+
+# for some reason this only works in the terminal with python3  cannot be python parrot.py
+
+
+prompt = "if you tell us who you are we can personalize your message"
+prompt += "\nWhat is your name?"
+
+name = input(prompt)
+print("\nHello "+ name)
+
+
+# for python2.7 you use raw_input instead of input...
