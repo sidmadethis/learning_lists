a9f3569ef1a9b13836a6427f36a6313943e24f99
sidmadethis <sidmadethis@gmail.com>
2017-04-27 23:34:22 -0600

    working with flags

--- counting.py
+++ counting.py
@@ -5,12 +5,22 @@ while current_number <=5:
 
 print("\n")
 
+
+active = True
 prompt="\nTell me something and I will repeat it back to you:"
 prompt += "\nEnter 'quit' to end the program."
+#
+# message = ""
+# while message != 'quit':
+#     message = input(prompt)
+#
+#     if message != 'quit':
+#         print(message)
 
-message = ""
-while message != 'quit':
+while active:
     message = input(prompt)
 
-    if message != 'quit':
+    if message == 'quit':
+        active = False
+    else:
         print(message)
