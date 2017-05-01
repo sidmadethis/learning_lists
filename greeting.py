4ebfb72bd2fc6f996860d17070fdb9538201d140
sidmadethis <sidmadethis@gmail.com>
2017-04-30 23:54:10 -0600

    functions with arguments

--- greeting.py
+++ greeting.py
@@ -1,5 +1,14 @@
-def greet_user():
-    """Display a simple greeting"""
-    print("Hello")
+# def greet_user():
+#     """Display a simple greeting"""
+#     print("Hello")
+#
+# greet_user()
 
-greet_user()
+
+# the triple quote is a docstring, used for documentation by python
+
+def greet_user(username):
+    """display a simple greeting"""
+    print("Hello, " + username.title()+"!")
+
+greet_user('jesse')
