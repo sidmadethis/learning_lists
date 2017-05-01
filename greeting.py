03d4e5fc70deb7a15ed33dbb9332bb1c8339f66f
sidmadethis <sidmadethis@gmail.com>
2017-05-01 00:02:27 -0600

    positional arguments

--- greeting.py
+++ greeting.py
@@ -20,3 +20,12 @@ def favorite_book(bookname):
     print("One of my favorite books is "+ bookname.title())
 
 favorite_book("Alice in Wonderland")
+
+print("\n")
+
+# positional arguments looks at order of arguments
+def describe_pet(animal_type, pet_name):
+    print("\nI have a "+ animal_type+".")
+    print("My "+animal_type+"'s name is "+ pet_name.title()+".")
+describe_pet("llama", "lloyd")
+describe_pet("dog", "chester")
