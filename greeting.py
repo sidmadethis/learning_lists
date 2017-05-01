65e8d41007a6bd82d6b640e3eec798ce7b22a00f
sidmadethis <sidmadethis@gmail.com>
2017-05-01 10:08:43 -0600

    keyword arguments

--- greeting.py
+++ greeting.py
@@ -24,8 +24,12 @@ favorite_book("Alice in Wonderland")
 print("\n")
 
 # positional arguments looks at order of arguments
-def describe_pet(animal_type, pet_name):
+def describe_pet(pet_name, animal_type = 'dog'):
     print("\nI have a "+ animal_type+".")
     print("My "+animal_type+"'s name is "+ pet_name.title()+".")
-describe_pet("llama", "lloyd")
-describe_pet("dog", "chester")
+# describe_pet("llama", "lloyd")
+# describe_pet("dog", "chester")
+
+# keyword argument
+describe_pet(pet_name = 'willie')
+# note the order of parameters had to be changed
