cb54674034c7388f76c1ef1ab690d168b4d0bc60
sidmadethis <sidmadethis@gmail.com>
2017-05-03 17:29:13 -0600

    more work on functions

--- formatted_name.py
+++ formatted_name.py
@@ -5,3 +5,7 @@ def get_formatted_name(first_name, last_name):
 
 musician = get_formatted_name('jimi', 'hendrix')
 print(musician)
+
+
+musician2 = get_formatted_name('aaaa', 'bbbb')
+print(musician2)
