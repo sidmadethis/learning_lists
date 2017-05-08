fcad2579279fbbcb2489c1aafca232bd33cad97c
sidmadethis <sidmadethis@gmail.com>
2017-05-08 09:14:16 -0600

    modules and import

---
+++ making_pizzas2.py
@@ -0,0 +1,7 @@
+import pizza2
+
+pizza2.make_pizza(16, 'pepperoni')
+pizza2.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+
+
+# this was to show how modules work. the import statement looks for pizza2.py, and sees the function make_pizza.  when you run making_pizzas2 the import statement works behind the scenes and you get the correct output.  similar to import in node?
